4aad8ee9640edb2e9fe89dae30f9787c9ffc1b4f
Jupplsix <37137000+Jupplsix@users.noreply.github.com>
2018-03-11 11:18:19 +1300

    Improved mobile responsiveness, edited JS file

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 var landing = document.querySelector("#landing");
 
 function resizeLanding() {
-	var height = window.innerHeight + "px";
+	var width = window.innerWidth;
+	if(width >= 680) {
+		var height = window.innerHeight;
+		height = height + "px"
+		landing.style.height = height;
+		console.log(height);
+	}
+}
+
+function resizeLandingLoad() {
+	var height = window.innerHeight;
+	height = height + "px"
 	landing.style.height = height;
 	console.log(height);
 }
 
 window.addEventListener('resize', resizeLanding); 
-window.addEventListener('load', resizeLanding);
+window.addEventListener('load', resizeLandingLoad);
